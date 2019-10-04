a728aadad28a6f0f5c040e8acc4f78191ae41840
ozay <ozay@umich.edu>
2019-10-03 22:23:34 -0400

    Initial Commit

---
+++ project2a/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project2a
+//
+//  Created by Oliver Zay on 10/3/19.
+//  Copyright © 2019 Oliver Zay. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
